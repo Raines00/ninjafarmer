82e4cec996fdfe78bfc60f481f32c07675984437
Raines00 <95763905+Raines00@users.noreply.github.com>
2021-12-09 16:15:52 +0000

    Update settings.py

--- ninjafarmer/settings.py
+++ ninjafarmer/settings.py
@@ -127,8 +127,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 
 django_heroku.settings(locals())
